aa2c753b9cff6f989009a943fc9a1c8647bbc0ad
Corey Furmanski <furmanskic@live.com>
2013-05-20 03:47:09 -0400

    fixed neg fix to how xcom wanted

--- hnh-neg-fix.lisp
+++ hnh-neg-fix.lisp
@@ -29,7 +29,7 @@
                   (t (return-from get-neg-opts (values (cons off sz) opt))))
                 (return-from get-neg-opts (values (cons off sz) nil)))))))
     
-(defun fix-neg (file neg-opts)
+(defun fix-neg-by-filter (file neg-opts)
   (with-open-file (fd file :element-type '(unsigned-byte 8))
     (when (equalp (str->ub "Haven Resource 1")
                   (read-times fd (length "Haven Resource 1")))
@@ -73,10 +73,77 @@
   (rename-file (concatenate 'string (namestring file) ".new")
                file))
 
+(defun m2s (pair)
+  (cons (- (* (car pair) 2) (* (cdr pair) 2))
+        (+ (car pair) (cdr pair))))
+
+(defun fix-neg-by-m2s (file)
+  (let ((tmp-fn (concatenate 'string (namestring file) ".tmp")))
+    (with-open-file (fd file :element-type '(unsigned-byte 8))
+      (when (equalp (str->ub "Haven Resource 1")
+                    (read-times fd (length "Haven Resource 1")))
+        (with-open-file (nfd tmp-fn :direction :output :element-type '(unsigned-byte 8))
+          (write-sequence (str->ub "Haven Resource 1") nfd)
+          (write-sequence (read-times fd *version-size*) nfd)
+        (loop
+             (let ((buf (loop 
+                           for ch = (read-byte fd nil :eof)
+                           until (or (eq ch :eof)
+                                     (= ch 0))
+                           do (write-byte ch nfd)
+                           collect ch)))
+               (when (zerop (length buf))
+                 (return))
+               (write-byte 0 nfd)
+               (let* ((rsz (read-times fd *layer-len-size*))
+                      (size (ubarr->uint rsz)))
+                 (write-sequence (int->ubarr size 4) nfd)
+                 (if (not (equalp buf '(110 101 103)))
+                     (write-sequence (read-times fd size) nfd)
+                     (let* ((neg-data (read-times fd size))
+                            (offset-x (make-array 2
+                                                :element-type '(unsigned-byte 8)
+                                                :displaced-to neg-data 
+                                                :displaced-index-offset 4))
+                            (offset-y (make-array 2
+                                                :element-type '(unsigned-byte 8)
+                                                :displaced-to neg-data 
+                                                :displaced-index-offset 6))
+                            (size-x (make-array 2
+                                              :element-type '(unsigned-byte 8)
+                                              :displaced-to neg-data 
+                                              :displaced-index-offset 8))
+                            (size-y (make-array 2
+                                              :element-type '(unsigned-byte 8)
+                                              :displaced-to neg-data 
+                                              :displaced-index-offset 10))
+                            (roff-x (ubarr->int offset-x))
+                            (roff-y (ubarr->int offset-y))
+                            (rsz-x (ubarr->int size-x))
+                            (rsz-y (ubarr->int size-y))
+                            (new-off (m2s (cons roff-x roff-y)))
+                            (new-sz (m2s (cons rsz-x rsz-y)))
+                            (new-off-x (int->ubarr (car new-off) 2))
+                            (new-off-y (int->ubarr (cdr new-off) 2))
+                            (new-sz-x (int->ubarr (car new-sz) 2))
+                            (new-sz-y (int->ubarr (cdr new-sz) 2)))
+                       (setf (aref neg-data 4) (aref new-off-x 0))
+                       (setf (aref neg-data 5) (aref new-off-x 1))
+                       (setf (aref neg-data 6) (aref new-off-y 0))
+                       (setf (aref neg-data 7) (aref new-off-y 1))
+                       (setf (aref neg-data 8) (aref new-sz-x 0))
+                       (setf (aref neg-data 9) (aref new-sz-x 1))
+                       (setf (aref neg-data 10) (aref new-sz-y 0))
+                       (setf (aref neg-data 11) (aref new-sz-y 1))
+                       (write-sequence neg-data nfd)))))))))
+    (delete-file file)
+    (rename-file tmp-fn file)))
+   
 (defun fix-negs (res-folder filter-file)
   "Fixes the neg layers of select files based on a filter file"
   (let ((name "")
         (next-name nil))
+    ;;filter run
     (with-open-file (fd filter-file)
       (while (not (eq name :eof))
         (if next-name
@@ -92,5 +159,9 @@
             (when file
               (multiple-value-bind (opts nn)
                   (get-neg-opts fd)
-                (fix-neg file opts)
-                (setf next-name nn)))))))))
+                (fix-neg-by-filter file opts)
+                (setf next-name nn)))))))
+    ;;global run
+    (let ((files (solve-files-1 (concatenate 'string res-folder "/"))))
+      (dolist (file files)
+        (fix-neg-by-m2s file)))))
